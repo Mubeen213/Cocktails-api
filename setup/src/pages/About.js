383a1b6cb910e36dd64ca9598daac42e7763d004
Mubeen <mdmubeen1307@gmail.com>
2022-04-26 17:45:11 +0530

    Context api

--- setup/src/pages/About.js
+++ setup/src/pages/About.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quam
+        nam vel sapiente dolore adipisci facilis cupiditate perspiciatis rerum.
+        Vel eius incidunt placeat quod harum eligendi, quibusdam ipsam est rem.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
